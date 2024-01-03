4982eaba75518da83efd7cf46e82f05f3938253a
ADtheProgrammer <133758855+ADtheProgrammer@users.noreply.github.com>
2024-01-03 19:30:10 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA5CeIzyMB36eIul4NMOVE3rgac7e_M4k8",
+    authDomain: "c101chatapp.firebaseapp.com",
+    databaseURL: "https://c101chatapp-default-rtdb.firebaseio.com",
+    projectId: "c101chatapp",
+    storageBucket: "c101chatapp.appspot.com",
+    messagingSenderId: "981474843166",
+    appId: "1:981474843166:web:c34f6dc2deb86a785c2c0f"
+  };
+  
